4af6ccfc30ef7656c639182c93db911ae2c9ce0f
GrégoireMorane <gh.morane@gmail.com>
2019-06-25 11:12:43 +0200

    feature(Title): styledComponents title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,1 +1,17 @@
-// title component
+import React from "react";
+import styled from "styled-components";
+
+const TypoStyled = styled.h1`
+  font-size: ${props => props.fontSize || "14px"};
+  line-height: ${props => props.lineHeight || "14px"};
+  color: ${props => props.color || "#7FFF00"};
+  text-align: ${props => props.textAlign || "left"};
+`;
+
+const Typo = ({ text, color, textAlign, lineHeight, fontSize }) => (
+  <TypoStyled color={color} align={textAlign} size={fontSize} line={lineHeight}>
+    {text}
+  </TypoStyled>
+);
+
+export { Typo };
