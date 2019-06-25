d946c339245a4d7903d3a448bec9164969de3eca
alex <smirnoff.awa@gmail.com>
2019-06-25 16:39:33 +0200

    add horloge dans le salon

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,19 +1,37 @@
 import React from "react";
 import { Wrapper } from '../../components/Wrapper/index'
 import Header from "../Header";
-import flatBackground from '../../assets/flatBackground.jpg'
+import flatBackground from '../../assets/appartement2.jpg'
 import ChairSvgfrom from '../../components/SVG/ChairSvg'
+import Center from 'react-center';
+import HorlogeSvg from "../../components/SVG/HorlogeSvg";
 
 class Home extends React.Component {
+  state = {
+    screenWidth: "",
+    screenHeight: ""
+  };
+
+  componentWillMount() {
+    this.setState({
+      screenHeight: window.innerHeight + "px",
+      screenWidth: window.innerWidth + "px"
+    });
+  }
   render() {
+
     return (
       <Wrapper>
         <Header />
-        <Wrapper backgroundImage={flatBackground} height="1000px" >
-          <Wrapper  position="absolute" top="620px" left="260px">
-            <ChairSvgfrom width={450} height={450}/>
+        <Center>
+          <img src={flatBackground} alt="appartement" />
+          <Wrapper position="absolute" top="680px" left="390px">
+            <ChairSvgfrom width={416} height={370} />
           </Wrapper>
-        </Wrapper>
+          <Wrapper position="absolute" top="280px" left="790px">
+            <HorlogeSvg width={416} height={370} />
+          </Wrapper>
+        </Center>
       </Wrapper>
     );
   }
