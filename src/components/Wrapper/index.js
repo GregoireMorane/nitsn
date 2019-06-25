1e4dcf6e37d3f1db0c0b408841ede58c3757ed8c
alex <smirnoff.awa@gmail.com>
2019-06-25 14:03:13 +0200

    gros push en master comme un sale

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -4,14 +4,18 @@ import styled from "styled-components";
 const WrapperStyled = styled.div`
   height: ${props => props.height || "14px"};
   width: ${props => props.width || "100%"};
+  padding: ${props => props.width || "100%"};
   background-color: ${props => props.backgroundColor || "#fff"};
+  background-size: ${props => props.backgroundColor || "cover"};
+  background-image: url("${props=> props.backgroundImage || null}");
 `;
 
-const Wrapper = ({ children, backgroundColor, width, height }) => (
+const Wrapper = ({ children, backgroundColor, width, height,backgroundImage }) => (
   <WrapperStyled
     height={height}
     width={width}
     backgroundColor={backgroundColor}
+    backgroundImage={backgroundImage}
   >
     {children}
   </WrapperStyled>
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,20 @@
 import React from "react";
-
+import { Wrapper } from '../../components/Wrapper/index'
 import Header from "../Header";
+import flatBackground from '../../assets/flatBackground.jpg'
+import ChairSvgfrom from '../../components/SVG/ChairSvg'
 
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <Wrapper>
         <Header />
-      </div>
+        <Wrapper backgroundImage={flatBackground} height="1000px">
+          <Wrapper>
+            <ChairSvgfrom width={300} height={700} />
+          </Wrapper>
+        </Wrapper>
+      </Wrapper>
     );
   }
 }
