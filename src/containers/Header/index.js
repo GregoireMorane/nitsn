34b6e4502f4582f04b30615d8738257b086357a5
GrégoireMorane <gh.morane@gmail.com>
2019-06-25 13:40:05 +0200

    feature(header): style

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 
-import { Typo } from "../../components/Title";
+import { Typo } from "../../components/Typo";
 import { Wrapper } from "../../components/Wrapper";
-import { Button } from "../../components/Button";
 
 class Header extends React.Component {
   handleClick = () => {
@@ -10,13 +9,14 @@ class Header extends React.Component {
   };
   render() {
     return (
-      <Wrapper height="100px" backgroundColor="#4682B4">
-        <Typo text="test" />
-        <Button
-          text="clique moi"
-          width="60"
-          height="30"
-          action={this.handleClick}
+      <Wrapper height="100px" backgroundColor="#fff">
+        <Typo
+          text="nitsn"
+          color="black"
+          textAlign="left"
+          fontSize="50px"
+          padding="35px 0px 0px 20px"
+          letterSpacing="7px"
         />
       </Wrapper>
     );
