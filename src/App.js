790294b21dc0418120a789cad537593bbb9c40a0
alex <smirnoff.awa@gmail.com>
2019-06-25 11:11:57 +0200

    import svg component to test modals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import ChairSvg from './components/SVG/ChairSvg';
+import TestExport from './components/SVG/TestExport';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ChairSvg width={600} height={700}/>
+     <TestExport width={600} height={700}/>
     </div>
   );
 }
