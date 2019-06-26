015604baf9448e09b8b5a811a655ac550d14495e
alex <smirnoff.awa@gmail.com>
2019-06-26 17:52:58 +0200

    feature(Personnalisation): filters chair parts

--- src/components/Enssemble/index.js
+++ src/components/Enssemble/index.js
@@ -7,14 +7,26 @@ import Modal from "../../containers/Modal";
 
 class Enssemble extends React.Component {
   state = {
-    shouldRenderModal: false
+    shouldRenderModal: false,
+    dossierSelectedColor: "#B0E0E6",
+    dossierLateralSelectedColor: "#B0E0E6",
+    assiseSelectedColor: "#B0E0E6",
   };
   shouldRenderModal = () => {
     this.setState({ shouldRenderModal: !this.state.shouldRenderModal });
   };
+  setDossierColor = (color) => {
+    this.setState({ dossierSelectedColor: color })
+  }
+  setDossierLateralColor = (color) => {
+    this.setState({ dossierLateralSelectedColor: color })
+  }
+  setAssiseColor = (color) => {
+    this.setState({ assiseSelectedColor: color })
+  }
   renderItem = () => {
     if (this.state.shouldRenderModal === true) {
-      return <Modal shouldRenderModal={this.shouldRenderModal} />;
+      return <Modal shouldRenderModal={this.shouldRenderModal} setDossierColor={this.setDossierColor} setDossierLateralColor={this.setDossierLateralColor} setAssiseColor={this.setAssiseColor} colorDossier={this.state.dossierSelectedColor} colorDossierLateral={this.state.dossierLateralSelectedColor} colorAssise={this.state.assiseSelectedColor} />;
     }
     return (
       <Wrapper
@@ -31,7 +43,7 @@ class Enssemble extends React.Component {
           left="180px"
           action={this.shouldRenderModal}
         >
-          <ChairSvg width={470} height={370} />
+          <ChairSvg width={470} height={370} dossierSelectedColor={this.state.dossierSelectedColor} dossierLateralSelectedColor={this.state.dossierLateralSelectedColor} assiseSelectedColor={this.state.assiseSelectedColor} />
         </Wrapper>
         <Wrapper position="absolute" top="20px" left="750px">
           <HorlogeSvg width={316} height={370} />
